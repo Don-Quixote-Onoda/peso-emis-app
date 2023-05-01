259d3e7ac9d3c0040d25c0ac093b7173fccb49ff
Don-Quixote-Onoda <jmnicolas4me@gmail.com>
2023-05-01 06:04:40 -0700

    update

--- resources/js/Pages/EmployerAdmin/Table.jsx
+++ resources/js/Pages/EmployerAdmin/Table.jsx
@@ -60,6 +60,20 @@ export default function JobPostingTable({
         );
     };
 
+    const priceBodyTemplate = (rowData) => {
+        const formatter = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'PHP',
+          
+            // These options are needed to round to whole numbers if that's what you want.
+            //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+            //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+          });
+          console.log();
+        return <>{formatter.format(rowData.salary)}</>;
+    };
+
+
     const isActiveStatus = (rowData) => {
         return (
             <span>{rowData.is_active == 1? 'Active': 'Not Active'}</span>
@@ -146,6 +160,7 @@ export default function JobPostingTable({
                             filter
                             filterPlaceholder="Search by name"
                             style={{ minWidth: "14rem" }}
+                            body={priceBodyTemplate}
                         />
                         <Column
                             field="vacancy_count"
