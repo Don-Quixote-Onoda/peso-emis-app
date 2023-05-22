acd005adf8dfd13313f6c055a64e652d24db8374
Pixeels <quintero.archie.o@gmail.com>
2023-05-22 15:47:06 +0800

    new

---
+++ public/build/assets/ApplicationLogo-ba68fffa.js
@@ -0,0 +1,1 @@
+import{a as p}from"./app-8152747c.js";function i(o){return p("img",{src:"/peso_logo.png",...o})}export{i as A};
--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,21 @@ export default defineConfig({
         }),
         react(),
     ],
+
+    build: {
+            rollupOptions: {
+                output:{
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                        }
+                    }
+                }
+            }
+        }
 });
+
+
 
 // import reactRefresh from '@vitejs/plugin-react-refresh';
 
