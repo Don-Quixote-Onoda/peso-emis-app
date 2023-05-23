1689ea8811410afeadcb2a12c40945aa9bd235fa
Pixeels <quintero.archie.o@gmail.com>
2023-05-24 05:52:24 +0800

    update

--- public/build/assets/ApplicationLogo-77dfa69c.js
+++
@@ -1,1 +0,0 @@
-import{a as p}from"./app-8a1df0f9.js";function i(o){return p("img",{src:"/peso_logo.png",...o})}export{i as A};
---
+++ public/build/assets/overlayservice.esm-8cc0dfa4.js
@@ -0,0 +1,1 @@
+import{E as r}from"./hooks.esm-5f47e4a9.js";var a=r();export{a as O};
