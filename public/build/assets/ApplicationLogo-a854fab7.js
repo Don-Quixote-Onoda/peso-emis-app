db54bc7d725f3588c2653773e05b2a645e143d97
Pixeels <quintero.archie.o@gmail.com>
2023-05-25 07:20:57 +0800

    message

--- public/build/assets/ApplicationLogo-a854fab7.js
+++
@@ -1,1 +0,0 @@
-import{a as p}from"./app-f4074e42.js";function i(o){return p("img",{src:"/peso_logo.png",...o})}export{i as A};
--- public/build/assets/Content-5c622351.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./app-f4074e42.js";function m({children:t}){return r("div",{class:"mt-36 !pl-5 mb-14 !p-0 !mr-0 ",children:t})}export{m as default};
